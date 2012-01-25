ae88c65bd0288dc21aa69292261f0dc52989fbfa
santosh <santosh@santosh-VirtualBox.(none)>
2012-01-25 15:07:25 +0530

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user=User.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @post }
+    end
+  end
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to :action => :new, notice: 'User is successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end  
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Food::Application.config.secret_token = '3a72b9c34fae3ee963d7a8827d2b85dfbb561d9c39c465057207f08e15a167d172e4efb613fa3824d4bc2ddc70fb9a46eb8a63d38eededa387a9f5d04ad796df'
