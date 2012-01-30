f45702f295cba0b6af3e241214e7713f376e8abe
santoshkosgi <santoshkosgi@gmail.com>
2012-01-30 15:16:18 +0530

    syntax errors removed

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,11 +9,6 @@ class UserSessionsController < ApplicationController
   # GET /user_sessions/new.json
   def new
     @user_session = UserSession.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user_session }
-    end
   end
 
   # GET /user_sessions/1/edit
@@ -23,15 +18,11 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
 
-    respond_to do |format|
       if @user_session.save
-        format.html { redirect_to :action => :index }
-        format.json { render json: @user_session, status: :created, location: @user_session }
+        redirect_to :action => "index"
       else
-        format.html { render action: "new" }
-        format.json { render json: @user_session.errors, status: :unprocessable_entity }
+        render action: "new"
       end
-    end
   end
 
   # PUT /user_sessions/1
@@ -43,9 +34,6 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.find
     @user_session.destroy
 
-    respond_to do |format|
-      format.html { redirect_to root_url }
-      format.json { head :ok }
-    end
+       redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,16 @@ class UsersController < ApplicationController
 
   def new
     @user=User.new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @post }
-    end
   end
 
   def create
     @user = User.new(params[:user])
-    respond_to do |format|
       if @user.save
         UserMailer.delay.registration_confirmation(@user)
-        format.html { redirect_to :action => :show, :id=> @user.id}
-        format.json { render json: @user, status: :created, location: @user }
+         redirect_to :action => "show", :id=> @user.id
       else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+         render action: "new"
       end
-    end
   end
 
   def show
@@ -34,9 +26,9 @@ class UsersController < ApplicationController
     @user = @current_user # makes our views "cleaner" and more consistent
     if @user.update_attributes(params[:user])
       flash[:notice] = "Account updated!"
-      redirect_to account_url
+      redirect_to :action => "index", :controller => "user_sessions"
     else
-      render :action => :edit
+      render :action => "edit"
     end
   end
 end
